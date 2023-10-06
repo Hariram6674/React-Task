4faf1c48ba45a4d13dc9a47301433f0aa7b628e6
Hariram6674 <hariram.pasupathy@2021@vitstudent.ac.in>
2023-10-06 16:04:34 +0400

    first

---
+++ src/components/Contact.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import Axios from 'axios';
+
+function Contact(){
+    const [name,setName] = useState("Shankar");
+    const [data,setData] = useState([]);
+    useEffect(()=>{ //sideeffects
+        Axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((res)=>{
+            if(res.status==200){
+                console.log(res.data);
+                setData(res.Data);
+            }
+            else{
+                Promise.reject();
+            }
+        })
+        .catch((err)=>
+            alert(err))
+    },[])
+    const handleClick = () =>{
+        setName("ravi");
+    }
+    const ContactDetails = () =>{
+        return data.map((val)=>{
+            return <p>{val.name} - {val.phone}</p>
+        })
+    }
+    return(
+        <div>
+            <h1>The owner of this page is {name} </h1>
+            <button onClick={handleClick}>Change owner</button>
+            {ContactDetails()}
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/components/F2.js
@@ -0,0 +1,9 @@
+export function Home(){
+    return ( <h1>Home components</h1>)
+}
+export function About(){
+    return ( <h1>About components</h1>)
+}
+export function Blog(){
+    return ( <h1>Blog components</h1>)
+}
