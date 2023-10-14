e2714c1f5025ffbc727c9e19346945132b4426c3
Hariram6674 <121010395+Hariram6674@users.noreply.github.com>
2023-10-14 20:41:34 +0400

    Delete src directory

--- src/components/F2.js
+++
@@ -1,9 +0,0 @@
-export function Home(){
-    return ( <h1>Home components</h1>)
-}
-export function About(){
-    return ( <h1>About components</h1>)
-}
-export function Blog(){
-    return ( <h1>Blog components</h1>)
-}
